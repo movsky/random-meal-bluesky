81c966d3907d829876764a94030ae7afd3df9767
mov <stefan.adamovsky@posteo.de>
2023-11-13 16:22:08 +0100

    implement logger to avoid duplicate meals directly after each other

--- src/Service/MealManager/MealManager.php
+++ src/Service/MealManager/MealManager.php
@@ -2,20 +2,36 @@
 
 namespace App\Service\MealManager;
 
-use App\Service\FeedLogger\FeedLogger;
+use App\Service\Logger\Logger;
 use App\Service\MealReader\Meal;
 use App\Service\MealReader\MealReader;
 
 class MealManager
 {
 
+    const MAX_ATTEMPTS = 5;
+
+    private int $attemptsCounter = 0;
+
     public function __construct(
-        private readonly MealReader $mealReader
+        private readonly MealReader $mealReader,
+        private readonly Logger $logger
     ) {}
 
     public function getMeal(): ?Meal
     {
-        return $this->mealReader->getRandom();
+        $meal = $this->mealReader->getRandom();
+        $log = $this->logger->getLogAsArray();
+
+        if (in_array($meal->getId(), $log) && $this->attemptsCounter < self::MAX_ATTEMPTS) {
+            $this->attemptsCounter++;
+
+            return $this->getMeal();
+        }
+
+        $this->logger->log($meal->getId());
+
+        return $meal;
     }
 
 }
